936b4751ece653e430017bb0ae0ff984cea7aa9a
James 2019 <jjalem9@3012-l29533-s.local>
2019-04-01 12:10:23 -0500

    Initial Commit

---
+++ Mobile Mapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile Mapper
+//
+//  Created by James Jalem 2019 on 4/1/19.
+//  Copyright © 2019 James Jalem 2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
